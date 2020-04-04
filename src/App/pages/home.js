7d7f457220147ba659e1459764604899b3efd4a4
Bhawana negi <bhawananegi0224@gmail.com>
2020-04-04 16:12:35 +0200

    Updated Styled components

--- src/App/pages/home.js
+++ src/App/pages/home.js
@@ -3,89 +3,92 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import styled from 'styled-components';
 
+const EmployeeCatalog = styled.div`
+display: flex;
+align-items: flex-start;
+flex-direction: column;
+max-width: 1170px;
+margin: auto;
+`;
+const Errormessage = styled.h4`
+color:red;
+font-size:1em
+`;
+
+const EmployeeHeading = styled.h1`
+margin:3% 0;
+text-transform:uppercase;
+color:#daa520;
+font-weight: bold;
+letter-spacing: 3px;
+text-shadow: 1px 2px 3px #000;
+`;
+
 class Home extends Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       list: [],
-      selectedTopic:0
+      selectedTopic: 0
     };
     this.handleSelectEmployee = this.handleSelectEmployee.bind(this);
 
   }
- 
-  handleSelectEmployee= (val) => {
+
+  handleSelectEmployee = (val) => {
     this.props.handleSelectEmployee({
       list: this.state.list,
-      selectedTopic:val
+      selectedTopic: val
     });
-  
+
   }
 
   // Fetch the list on first mount
   componentDidMount() {
     this.getList();
-   
+
   }
-  
+
 
   // // Retrieves the list of items from the Express app
   getList = () => {
     fetch('/api/getList')
-    .then(res => res.json())
-    .then(data => this.setState({ list:data}))
+      .then(res => res.json())
+      .then(data => this.setState({ list: data }))
   }
 
-  
+
+
   render() {
     const { list } = this.state;
 
-    const EmployeeCatalog= styled.div`
-    display: flex;
-    align-items: flex-start;
-    flex-direction: column;
-    max-width: 1170px;
-    margin: auto;
-  `;
-  const Errormessage= styled.h4`
-    color:red;
-    font-size:1em
-  `;
 
-  const EmployeeHeading = styled.h1`
-    margin:3% 0;
-    text-transform:uppercase;
-    color:#daa520;
-    font-weight: bold;
-    letter-spacing: 3px;
-    text-shadow: 1px 2px 3px #000;
-  `;
 
     return (
-    <div className="App">
-      <EmployeeHeading>Employee catalog</EmployeeHeading>
-      <div id="employeecatalog"> 
-      {list.length ? (
-          <EmployeeCatalog>
+      <div className="App">
+        <EmployeeHeading>Employee catalog</EmployeeHeading>
+        <div id="employeecatalog">
+          {list.length ? (
+            <EmployeeCatalog>
 
-            {/* Render the list of items */}
-            {list.map((item,index) => {
-              return(
-                <Link to={'./list'} key={index} id={item.id} className="EmployeeList" onClick={() => this.handleSelectEmployee(item.id)}>
-                {item.title}
-                </Link>
-              );
-            })}
-          </EmployeeCatalog>
-        ) : (
-          <div>
-            <Errormessage>No Employee details Found</Errormessage>
-          </div>
-        )
-      }
-            </div>
-    </div>
+              {/* Render the list of items */}
+              {list.map((item, index) => {
+                return (
+                  <Link to={'./list'} key={index} id={item.id} className="EmployeeList" onClick={() => this.handleSelectEmployee(item.id)}>
+                    {item.title}
+                  </Link>
+                );
+              })}
+            </EmployeeCatalog>
+          ) : (
+              <div>
+                <Errormessage>No Employee details Found</Errormessage>
+              </div>
+            )
+          }
+        </div>
+      </div>
     );
   }
 }
@@ -97,10 +100,10 @@ const mapStateToProps = (state) => {
 }
 const mapDispatchToProps = (dispatch) => {
   return {
-    handleSelectEmployee: (data) =>{
-   dispatch({ state: data, type: "displayrecords" })
-  }
+    handleSelectEmployee: (data) => {
+      dispatch({ state: data, type: "displayrecords" })
+    }
   }
 }
 
-export default connect(mapStateToProps,mapDispatchToProps)(Home);
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
--- src/App/pages/list.js
+++ src/App/pages/list.js
@@ -2,7 +2,37 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+const EmployeeWrapper = styled.div`
+    max-width:800px;
+    margin:auto;
+ `;
+const Errormessage = styled.h4`
+    color:red;
+    font-size:1em
+  `;
+const TextLabel = styled.span`
+    color:#262626;
+    font-size:1em;
+  `;
 
+const EmployeeHeading = styled.h1`
+ margin:3% 0;
+ text-transform:uppercase;
+ color:#daa520;
+ font-weight: bold;
+ letter-spacing: 3px;
+ text-shadow: 1px 2px 3px #000;
+`;
+const EmployeeLink = styled.div`
+   background-color: transparent;
+   color: #d2691e;
+   font-weight: 600;
+   padding: 5px 9px;
+   text-transform: capitalize;
+   font-size: 1.5em;
+   margin-left: 20px;
+   text-align:left
+ `;
 class List extends Component {
   // Initialize the state
   constructor(props) {
@@ -15,52 +45,22 @@ class List extends Component {
 
   render() {
     const { list } = this.props;
-    const EmployeeWrapper = styled.div`
-    max-width:800px;
-    margin:auto;
- `;
- const Errormessage= styled.h4`
-    color:red;
-    font-size:1em
-  `;
-  const TextLabel= styled.span`
-    color:#262626;
-    font-size:1em;
-  `;
 
-    const EmployeeHeading = styled.h1`
- margin:3% 0;
- text-transform:uppercase;
- color:#daa520;
- font-weight: bold;
- letter-spacing: 3px;
- text-shadow: 1px 2px 3px #000;
-`;
-    const EmployeeLink = styled.div`
-   background-color: transparent;
-   color: #d2691e;
-   font-weight: 600;
-   padding: 5px 9px;
-   text-transform: capitalize;
-   font-size: 1.5em;
-   margin-left: 20px;
-   text-align:left
- `;
     return (
       <div className="App">
         <Link to={'./'} className="BackBtn">Back</Link>
         <EmployeeHeading>Job details </EmployeeHeading>
-        
+
         {list.length ? (
           <EmployeeWrapper>
             <EmployeeLink>
-             <TextLabel>Job Title:</TextLabel> {list[0].title}
+              <TextLabel>Job Title:</TextLabel> {list[0].title}
             </EmployeeLink>
             <EmployeeLink >
-            <TextLabel>Job description:</TextLabel> {list[0].description}
+              <TextLabel>Job description:</TextLabel> {list[0].description}
             </EmployeeLink>
             <EmployeeLink>
-            <TextLabel>Job Employment type:</TextLabel> {list[0].employment_type}
+              <TextLabel>Job Employment type:</TextLabel> {list[0].employment_type}
             </EmployeeLink>
           </EmployeeWrapper>
         ) : (
